871e791c72a3de9206c2fd2c7847e11f9d6309f5
YALLICO OWNER <yallico2024@gmail.com>
2026-09-05 22:00:53 -0500

    Actualizar fun-freefire.js

--- plugins/fun-freefire.js
+++ plugins/fun-freefire.js
@@ -1,323 +1,121 @@
 let handler = async (m, { conn, command }) => {
 
-let listas = {
-'vs16': `߳₊🪭⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
+global.db = global.db || {}
+global.db.listas = global.db.listas || {}
+
+let idLista = `vs16_${m.chat}`
+
+// Reinicia la lista cada que mandan .vs16
+global.db.listas[idLista] = {
+    titulares: [],
+    suplentes: [],
+    maxTit: 4,
+    maxSup: 2
+}
+
+let lista = `߳₊🪭⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
 ╭ ꕀ ֹ
 ⌇ ⸝⸝ 🆚 𖥦 ﹕
 ⌇ ⸝⸝ ⏰ 𖥦 ﹕ _16 🇦🇷 ʾ 🇵🇪14_
 ╰ ☆⃞ 　 ʾ 　 ๑
 ╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
+⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕ 0/4
 ⌇🪭𐑞
 ⌇🪭𐑞
 ⌇🪭𐑞
-⌇🪭𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
+⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕ 0/2
 ⌇🎐𐑞
 ⌇🎐𐑞
-╰ ☆⃞ 　 ʾ 　 ๑
-\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``,
-
-'vs18': `߳₊🪭⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
-╭ ꕀ ֹ
-⌇ ⸝⸝ 🆚 𖥦 ﹕
-⌇ ⸝⸝ ⏰ 𖥦 ﹕ _18 🇦🇷 ʾ 🇵🇪16_
-╰ ☆⃞ 　 ʾ 　 ๑
-╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
-⌇🪭𐑞
-⌇🪭𐑞
-⌇🪭𐑞
-⌇🪭𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
-⌇🎐𐑞
-⌇🎐𐑞
-╰ ☆⃞ 　 ʾ 　 ๑
-\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``,
-
-'vs20': `߳₊🪭⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
-╭ ꕀ ֹ
-⌇ ⸝⸝ 🆚 𖥦 ﹕
-⌇ ⸝⸝ ⏰ 𖥦 ﹕ _20 🇦🇷 ʾ 🇵🇪18_
-╰ ☆⃞ 　 ʾ 　 ๑
-╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
-⌇🪭𐑞
-⌇🪭𐑞
-⌇🪭𐑞
-⌇🪭𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
-⌇🎐𐑞
-⌇🎐𐑞
-╰ ☆⃞ 　 ʾ 　 ๑
-\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``,
-
-'vs22': `߳₊🪭⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
-╭ ꕀ ֹ
-⌇ ⸝⸝ 🆚 𖥦 ﹕
-⌇ ⸝⸝ ⏰ 𖥦 ﹕ _22 🇦🇷 ʾ 🇵🇪20_
-╰ ☆⃞ 　 ʾ 　 ๑
-╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
-⌇🪭𐑞
-⌇🪭𐑞
-⌇🪭𐑞
-⌇🪭𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
-⌇🎐𐑞
-⌇🎐𐑞
-╰ ☆⃞ 　 ʾ 　 ๑
-\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``,
-
-'vs00': `߳₊🪭⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
-╭ ꕀ ֹ
-⌇ ⸝⸝ 🆚 𖥦 ﹕
-⌇ ⸝⸝ ⏰ 𖥦 ﹕ _00 🇦🇷 ʾ 🇵🇪22_
-╰ ☆⃞ 　 ʾ 　 ๑
-╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
-⌇🪭𐑞
-⌇🪭𐑞
-⌇🪭𐑞
-⌇🪭𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
-⌇🎐𐑞
-⌇🎐𐑞
-╰ ☆⃞ 　 ʾ 　 ๑
-\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``,
-
-'vs01': `߳₊🪭⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
-╭ ꕀ ֹ
-⌇ ⸝⸝ 🆚 𖥦 ﹕
-⌇ ⸝⸝ ⏰ 𖥦 ﹕ _01 🇦🇷 ʾ 🇵🇪23_
-╰ ☆⃞ 　 ʾ 　 ๑
-╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
-⌇🪭𐑞
-⌇🪭𐑞
-⌇🪭𐑞
-⌇🪭𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
-⌇🎐𐑞
-⌇🎐𐑞
-╰ ☆⃞ 　 ʾ 　 ๑
-\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``,
-
-'vsfem16': `߳₊💅⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
-╭ ꕀ ֹ
-⌇ ⸝⸝ 🆚 𖥦 ﹕
-⌇ ⸝⸝ ⏰ 𖥦 ﹕ _16 🇦🇷 ʾ 🇵🇪14_
-╰ ☆⃞ 　 ʾ 　 ๑
-╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
-⌇💋𐑞
-⌇💋𐑞
-⌇💋𐑞
-⌇💋𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
-⌇🌸𐑞
-⌇🌸𐑞
-╰ ☆⃞ 　 ʾ 　 ๑
-\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``,
-
-'vsfem18': `߳₊💅⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
-╭ ꕀ ֹ
-⌇ ⸝⸝ 🆚 𖥦 ﹕
-⌇ ⸝⸝ ⏰ 𖥦 ﹕ _18 🇦🇷 ʾ 🇵🇪16_
-╰ ☆⃞ 　 ʾ 　 ๑
-╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
-⌇💋𐑞
-⌇💋𐑞
-⌇💋𐑞
-⌇💋𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
-⌇🌸𐑞
-⌇🌸𐑞
-╰ ☆⃞ 　 ʾ 　 ๑
-\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``,
-
-'vsfem20': `߳₊💅⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
-╭ ꕀ ֹ
-⌇ ⸝⸝ 🆚 𖥦 ﹕
-⌇ ⸝⸝ ⏰ 𖥦 ﹕ _20 🇦🇷 ʾ 🇵🇪18_
-╰ ☆⃞ 　 ʾ 　 ๑
-╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
-⌇💋𐑞
-⌇💋𐑞
-⌇💋𐑞
-⌇💋𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
-⌇🌸𐑞
-⌇🌸𐑞
-╰ ☆⃞ 　 ʾ 　 ๑
-\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``,
-
-'vsfem22': `߳₊💅⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
-╭ ꕀ ֹ
-⌇ ⸝⸝ 🆚 𖥦 ﹕
-⌇ ⸝⸝ ⏰ 𖥦 ﹕ _22 🇦🇷 ʾ 🇵🇪20_
-╰ ☆⃞ 　 ʾ 　 ๑
-╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
-⌇💋𐑞
-⌇💋𐑞
-⌇💋𐑞
-⌇💋𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
-⌇🌸𐑞
-⌇🌸𐑞
-╰ ☆⃞ 　 ʾ 　 ๑
-\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``,
-
-'vsfem00': `߳₊💅⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
-╭ ꕀ ֹ
-⌇ ⸝⸝ 🆚 𖥦 ﹕
-⌇ ⸝⸝ ⏰ 𖥦 ﹕ _00 🇦🇷 ʾ 🇵🇪22_
-╰ ☆⃞ 　 ʾ 　 ๑
-╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
-⌇💋𐑞
-⌇💋𐑞
-⌇💋𐑞
-⌇💋𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
-⌇🌸𐑞
-⌇🌸𐑞
-╰ ☆⃞ 　 ʾ 　 ๑
-\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``,
-
-'vsfem01': `߳₊💅⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
-╭ ꕀ ֹ
-⌇ ⸝⸝ 🆚 𖥦 ﹕
-⌇ ⸝⸝ ⏰ 𖥦 ﹕ _01 🇦🇷 ʾ 🇵🇪23_
-╰ ☆⃞ 　 ʾ 　 ๑
-╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
-⌇💋𐑞
-⌇💋𐑞
-⌇💋𐑞
-⌇💋𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
-⌇🌸𐑞
-⌇🌸𐑞
-╰ ☆⃞ 　 ʾ 　 ๑
-\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``,
-
-'vsmasc16': `߳₊🔥⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
-╭ ꕀ ֹ
-⌇ ⸝⸝ 🆚 𖥦 ﹕
-⌇ ⸝⸝ ⏰ 𖥦 ﹕ _16 🇦🇷 ʾ 🇵🇪14_
-╰ ☆⃞ 　 ʾ 　 ๑
-╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
-⌇👑𐑞
-⌇👑𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
-⌇⚡𐑞
-⌇⚡𐑞
-╰ ☆⃞ 　 ʾ 　 ๑
-\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``,
-
-'vsmasc18': `߳₊🔥⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
-╭ ꕀ ֹ
-⌇ ⸝⸝ 🆚 𖥦 ﹕
-⌇ ⸝⸝ ⏰ 𖥦 ﹕ _18 🇦🇷 ʾ 🇵🇪16_
-╰ ☆⃞ 　 ʾ 　 ๑
-╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
-⌇👑𐑞
-⌇👑𐑞
-⌇👑𐑞
-⌇👑𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
-⌇⚡𐑞
-⌇⚡𐑞
-╰ ☆⃞ 　 ʾ 　 ๑
-\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``,
-
-'vsmasc20': `߳₊🔥⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
-╭ ꕀ ֹ
-⌇ ⸝⸝ 🆚 𖥦 ﹕
-⌇ ⸝⸝ ⏰ 𖥦 ﹕ _20 🇦🇷 ʾ 🇵🇪18_
-╰ ☆⃞ 　 ʾ 　 ๑
-╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
-⌇👑𐑞
-⌇👑𐑞
-⌇👑𐑞
-⌇👑𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
-⌇⚡𐑞
-⌇⚡𐑞
-╰ ☆⃞ 　 ʾ 　 ๑
-\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``,
-
-'vsmasc22': `߳₊🔥⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
-╭ ꕀ ֹ
-⌇ ⸝⸝ 🆚 𖥦 ﹕
-⌇ ⸝⸝ ⏰ 𖥦 ﹕ _22 🇦🇷 ʾ 🇵🇪20_
-╰ ☆⃞ 　 ʾ 　 ๑
-╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
-⌇👑𐑞
-⌇👑𐑞
-⌇👑𐑞
-⌇👑𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
-⌇⚡𐑞
-⌇⚡𐑞
-╰ ☆⃞ 　 ʾ 　 ๑
-\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``,
-
-'vsmasc00': `߳₊🔥⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
-╭ ꕀ ֹ
-⌇ ⸝⸝ 🆚 𖥦 ﹕
-⌇ ⸝⸝ ⏰ 𖥦 ﹕ _00 🇦🇷 ʾ 🇵🇪22_
-╰ ☆⃞ 　 ʾ 　 ๑
-╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
-⌇👑𐑞
-⌇👑𐑞
-⌇👑𐑞
-⌇👑𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
-⌇⚡𐑞
-⌇⚡𐑞
-╰ ☆⃞ 　 ʾ 　 ๑
-\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``,
-
-'vsmasc01': `߳₊🔥⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
-╭ ꕀ ֹ
-⌇ ⸝⸝ 🆚 𖥦 ﹕
-⌇ ⸝⸝ ⏰ 𖥦 ﹕ _01 🇦🇷 ʾ 🇵🇪23_
-╰ ☆⃞ 　 ʾ 　 ๑
-╭ ꕀ ֹ
-⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕
-⌇👑𐑞
-⌇👑𐑞
-⌇👑𐑞
-⌇👑𐑞
-⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕
-⌇⚡𐑞
-⌇⚡𐑞
 ╰ ☆⃞ 　 ʾ 　 ๑
 \`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``
-}
 
-let lista = listas[command]
-if (!lista) return m.reply('Comando no válido.\n\nMIxta:.vs16.vs18.vs20.vs22.vs00.vs01\nFEMENINA:.vsfem16.vsfem18.vsfem20.vsfem22.vsfem00.vsfem01\nMASCULINA:.vsmasc16.vsmasc18.vsmasc20.vsmasc22.vsmasc00.vsmasc01')
-
-await conn.sendMessage(m.chat, { text: lista }, { quoted: m })
-}
-
-handler.help = [
-'vs16 ( Lista Mixta Sur )','vs18 ( Lista Mixta Sur )','vs20 ( Lista Mixta Sur )','vs22 ( Lista Mixta Sur )','vs00 ( Lista Mixta Sur )','vs01 ( Lista Mixta Sur )',
-'vsfem16 ( Lista Femenina Sur )','vsfem18 ( Lista Femenina Sur )','vsfem20 ( Lista Femenina Sur )','vsfem22 ( Lista Femenina Sur )','vsfem00 ( Lista Femenina Sur )','vsfem01 ( Lista Femenina Sur )',
-'vsmasc16 ( Lista Masculina Sur )','vsmasc18 ( Lista Masculina Sur )','vsmasc20 ( Lista Masculina Sur )','vsmasc22 ( Lista Masculina Sur )','vsmasc00 ( Lista Masculina Sur )','vsmasc01 ( Lista Masculina Sur )'
+let buttons = [
+    { buttonId: `tit_${idLista}`, buttonText: { displayText: '🎀 Jugadora' }, type: 1 },
+    { buttonId: `sup_${idLista}`, buttonText: { displayText: '🌸 Suplente' }, type: 1 },
+    { buttonId: `out_${idLista}`, buttonText: { displayText: '❌ Salir' }, type: 1 }
 ]
-handler.tags = ['ff']
-handler.command = /^(vs16|vs18|vs20|vs22|vs00|vs01|vsfem16|vsfem18|vsfem20|vsfem22|vsfem00|vsfem01|vsmasc16|vsmasc18|vsmasc20|vsmasc22|vsmasc00|vsmasc01)$/i
 
+await conn.sendMessage(m.chat, {
+    text: lista,
+    footer: 'Toca para anotarte al vs16',
+    buttons: buttons,
+    headerType: 1
+}, { quoted: m })
+
+}
+
+handler.help = ['vs16']
+handler.tags = ['ff']
+handler.command = /^vs16$/i
 export default handler
+
+// ===== HANDLER PARA LOS BOTONES =====
+export async function before(m, { conn }) {
+    if (!m.message?.buttonsResponseMessage) return
+    let id = m.message.buttonsResponseMessage.selectedButtonId
+    let user = m.sender
+    let name = await conn.getName(user)
+
+    if(!id.startsWith('tit_') && !id.startsWith('sup_') && !id.startsWith('out_')) return
+    
+    let idLista = id.split('_').slice(1).join('_')
+    let lista = global.db?.listas?.[idLista]
+    if(!lista) return conn.reply(m.chat, '⚠️ Esta lista ya expiró. Manda .vs16 de nuevo', m)
+
+    // Quitar de ambas listas primero por si cambia
+    lista.titulares = lista.titulares.filter(v => v.id !== user)
+    lista.suplentes = lista.suplentes.filter(v => v.id !== user)
+
+    if(id.startsWith('tit_')){
+        if(lista.titulares.length < lista.maxTit){
+            lista.titulares.push({id: user, name})
+            await conn.reply(m.chat, `🎀 @${user.split('@')[0]} se anotó como TITULAR`, m, { mentions: [user] })
+        } else {
+            await conn.reply(m.chat, `⚠️ Ya hay 4 titulares. Usa 🌸 Suplente`, m)
+        }
+    }
+    
+    if(id.startsWith('sup_')){
+        if(lista.suplentes.length < lista.maxSup){
+            lista.suplentes.push({id: user, name})
+            await conn.reply(m.chat, `🌸 @${user.split('@')[0]} se anotó como SUPLENTE`, m, { mentions: [user] })
+        } else {
+            await conn.reply(m.chat, `⚠️ Ya hay 2 suplentes`, m)
+        }
+    }
+    
+    if(id.startsWith('out_')){
+        await conn.reply(m.chat, `❌ @${user.split('@')[0]} salió de la lista`, m, { mentions: [user] })
+    }
+
+    // Actualizar lista
+    let textoTit = lista.titulares.map((v,i) => `⌇🪭 ${i+1}. @${v.id.split('@')[0]}`).join('\n')
+    let textoSup = lista.suplentes.map((v,i) => `⌇🎐 ${i+1}. @${v.id.split('@')[0]}`).join('\n')
+    
+    let texto = `߳₊🪭⋆.˚ 𝟦𝑽𝑺4 𝑺𝑼𝑹 ꒱ ˖ׄ ୭
+╭ ꕀ ֹ
+⌇ ⸝⸝ 🆚 𖥦 ﹕
+⌇ ⸝⸝ ⏰ 𖥦 ﹕ _16 🇦🇷 ʾ 🇵🇪14_
+╰ ☆⃞ 　 ʾ 　 ๑
+╭ ꕀ ֹ
+⌇ ◟✦ 𓏼𝑻𝑰𝑻𝑼𝑳𝑨𝑹𝑬𝑺﹕ ${lista.titulares.length}/4
+${textoTit || '⌇🪭𐑞'}
+⌇ ◟✦ 𓏼𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬𝑺﹕ ${lista.suplentes.length}/2
+${textoSup || '⌇🎐𐑞'}
+╰ ☆⃞ 　 ʾ 　 ๑
+\`｡⁖. 𝑷𝒖𝒏𝒕𝒖𝒂𝒍𝒊𝒅𝒂𝒅 | 𝑺𝒊𝒏 𝒍𝒂𝒈 | 𝑹𝒆𝒔𝒑𝒆𝒕𝒐 ⁖｡\``
+
+    let buttons = [
+        { buttonId: `tit_${idLista}`, buttonText: { displayText: '🎀 Jugadora' }, type: 1 },
+        { buttonId: `sup_${idLista}`, buttonText: { displayText: '🌸 Suplente' }, type: 1 },
+        { buttonId: `out_${idLista}`, buttonText: { displayText: '❌ Salir' }, type: 1 }
+    ]
+
+    await conn.sendMessage(m.chat, {
+        text: texto,
+        footer: 'Toca para anotarte al vs16',
+        buttons: buttons,
+        headerType: 1,
+        mentions: [...lista.titulares.map(v=>v.id),...lista.suplentes.map(v=>v.id)]
+    })
+}
