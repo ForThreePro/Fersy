b6e49903e75951b813ea05d3a91a1b24cec8e09b
YALLICO OWNER <yallico2024@gmail.com>
2026-09-08 17:26:38 -0500

    Actualizar _ship.js

--- plugins/_ship.js
+++ plugins/_ship.js
@@ -8,8 +8,11 @@ let handler = async (m, { conn, participants }) => {
     let user2 = members[Math.floor(Math.random() * members.length)]
     while(user1 === user2) user2 = members[Math.floor(Math.random() * members.length)]
 
-    await conn.sendMessage(m.chat, { 
-        text: `💘 *CALCULANDO SHIP...* 💘\n\n@${user1.split('@')[0]} + @${user2.split('@')[0]}`,
+    let name1 = '@' + user1.split('@')[0]
+    let name2 = '@' + user2.split('@')[0]
+
+    await conn.sendMessage(m.chat, {
+        text: `💘 *CALCULANDO SHIP...* 💘\n\n${name1} + ${name2}`,
         mentions: [user1, user2]
     })
 
@@ -20,18 +23,19 @@ let handler = async (m, { conn, participants }) => {
         let background = 'https://files.evogb.win/7BY3Yv.jpg'
 
         let url = `https://api.stellarwa.xyz/generate/ship?avatar1=${encodeURIComponent(avatar1)}&avatar2=${encodeURIComponent(avatar2)}&background=${encodeURIComponent(background)}&key=proyectsV2`
-        
+
         let res = await fetch(url)
         if(!res.ok) throw await res.text()
         let buffer = await res.buffer()
 
         let porcentaje = Math.floor(Math.random() * 101)
-        let texto = porcentaje < 30 ? '💔 Ni con magia...' : porcentaje < 60 ? '💛 Tal vez...' : porcentaje < 80 ? '❤️ Se ven lindos' : '💖 CASORIO YA!!'
+        let texto = porcentaje < 30? '💔 Ni con magia...' : porcentaje < 60? '💛 Tal vez...' : porcentaje < 80? '❤️ Se ven lindos' : '💖 CASORIO YA!!'
 
-        let caption = `🐱 *GARFIELD SHIP* 🐱\n\n@${user1.split('@')[0]} + @${user2.split('@')[0]}\n\n*Compatibilidad: ${porcentaje}%*\n${texto}`
+        // USAMOS name1 y name2 con el @ para que WhatsApp los pinte
+        let caption = `🐱 *GARFIELD SHIP* 🐱\n\n${name1} + ${name2}\n\n*Compatibilidad: ${porcentaje}%*\n${texto}`
 
-        await conn.sendFile(m.chat, buffer, 'ship.png', caption, m, { 
-            mentions: [user1, user2] // AQUÍ ESTÁ EL FIX
+        await conn.sendFile(m.chat, buffer, 'ship.png', caption, m, {
+            mentions: [user1, user2] // Pasamos los jid completos
         })
 
     } catch (e) {
