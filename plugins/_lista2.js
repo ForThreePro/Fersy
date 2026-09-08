0f0131875a43f4ad8aa529a75dec0fc06f3659b2
YALLICO OWNER <yallico2024@gmail.com>
2026-09-08 18:36:38 -0500

    Actualizar _lista2.js

--- plugins/_lista2.js
+++ plugins/_lista2.js
@@ -6,15 +6,13 @@ if (!fs.existsSync(db)) fs.writeFileSync(db, JSON.stringify([]))
 
 let handler = async (m, { conn, command }) => {
     if (command === 'borrarlista') {
-        if (!m.isAdmin && !m.isOwner) return m.reply('❌ *SIN PERMISO*\nSolo admins pueden usar este comando')
-        
         fs.writeFileSync(db, JSON.stringify([]))
         return m.reply('🗑️ *LISTA BORRADA COMPLETAMENTE*\nSe eliminaron todos los registros de Lunes a Sábado')
     }
 }
 
 handler.help = ['borrarlista']
-handler.tags = ['admin']
+handler.tags = ['group']
 handler.command = /^(borrarlista)$/i
 handler.group = true
 
