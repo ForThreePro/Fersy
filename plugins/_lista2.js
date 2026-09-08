02f76a05b9501388dba276be6439211590baccc9
YALLICO OWNER <yallico2024@gmail.com>
2026-09-08 18:30:32 -0500

    Actualizar _lista2.js

--- plugins/_lista2.js
+++ plugins/_lista2.js
@@ -4,17 +4,19 @@ let db = './src/database/lista.json'
 if (!fs.existsSync(db)) fs.writeFileSync(db, JSON.stringify([]))
 
 let handler = async (m, { conn, command }) => {
-    if (command === 'borrartodo') {
-        if (!m.isAdmin && !m.isOwner) return m.reply('❌ Solo admins pueden borrar la lista')
+    if (command === 'borrarlista') {
+        // Solo admins y owner
+        if (!m.isAdmin && !m.isOwner) return m.reply('❌ *SIN PERMISO*\nSolo admins pueden usar este comando')
+        
         fs.writeFileSync(db, JSON.stringify([]))
-        return m.reply('🗑️ *LISTA BORRADA COMPLETA*')
+        return m.reply('🗑️ *LISTA BORRADA COMPLETAMENTE*\nSe eliminaron todos los registros de Lunes a Sábado')
     }
 }
 
-handler.help = ['borrartodo']
+handler.help = ['borrarlista']
 handler.tags = ['admin']
-handler.command = /^(borrartodo)$/i
+handler.command = /^(borrarlista)$/i // <-- Cambiado aquí
 handler.group = true
-handler.admin = true
+handler.admin = true // Esto ya bloquea que no-admins lo usen
 
 export default handler
