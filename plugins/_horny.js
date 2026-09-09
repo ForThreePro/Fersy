d1a48f178ab255ec550ec01dde0682b323583533
YALLICO OWNER <yallico2024@gmail.com>
2026-09-09 09:29:16 -0500

    Actualizar _horny.js

--- plugins/_horny.js
+++ plugins/_horny.js
@@ -2,12 +2,18 @@ let handler = async (m, { conn }) => {
     let who = m.mentionedJid[0]? m.mentionedJid[0] : m.quoted? m.quoted.sender : m.sender
     let name = await conn.getName(who)
 
-    // Si tiene foto usa la de él. Si no, usa la por defecto
-    let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://files.evogb.win/E2yVdA.jpg')
+    // URL por defecto si no tiene foto
+    let defaultAvatar = 'https://files.evogb.win/E2yVdA.jpg'
 
-    // TU KEY DE STELLARWA
+    // Intentamos sacar la foto. Si falla, usamos la default
+    let pp = await conn.profilePictureUrl(who, 'image').catch(() => defaultAvatar)
+
+    // VALIDACIÓN EXTRA: por si devuelve "" o null
+    if (!pp ||!pp.startsWith('http')) {
+        pp = defaultAvatar
+    }
+
     let key = 'garfield-vip'
-
     let url = `https://api.stellarwa.xyz/generate/horny?avatar=${encodeURIComponent(pp)}&key=${key}`
 
     await conn.sendFile(m.chat, url, 'horny.jpg', `*${name}* está así ahora mismo 😏🔥`, m)
