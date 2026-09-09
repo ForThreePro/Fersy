29eff7a19825a875a63077030bd4cf8248a9fc23
YALLICO OWNER <yallico2024@gmail.com>
2026-09-09 16:12:40 -0500

    Actualizar menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -6,12 +6,8 @@ let handler = async (m, { conn, usedPrefix }) => {
 
   let taguser = m.mentionedJid && m.mentionedJid[0]? m.mentionedJid[0] : m.quoted? m.quoted.sender : m.sender
 
-  // Rota las 2 fotos
-  const images = [
-    'https://files.evogb.win/QFXQtu.jpg',
-    'https://files.evogb.win/E2yVdA.jpg'
-  ]
-  let img = { url: images[Math.floor(Math.random() * images.length)] }
+  // FOTO GLOBAL
+  let img = { url: global.botimg }
 
   let uptime = process.uptime() * 1000
   let _uptime = clockString(uptime)
