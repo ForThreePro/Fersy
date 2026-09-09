8b19d603d7e486217aa743cbbace5eb872ea5e86
YALLICO OWNER <yallico2024@gmail.com>
2026-09-09 09:33:58 -0500

    Actualizar _horny.js

--- plugins/_horny.js
+++ plugins/_horny.js
@@ -1,6 +1,8 @@
 let handler = async (m, { conn }) => {
     let who = m.mentionedJid[0]? m.mentionedJid[0] : m.quoted? m.quoted.sender : m.sender
-    let name = await conn.getName(who)
+
+    // ARREGLO 1: Sacar nombre bien. Si falla usa el número
+    let name = await conn.getName(who).catch(() => who.split('@')[0])
 
     // URL por defecto
     let defaultAvatar = 'https://files.evogb.win/E2yVdA.jpg'
@@ -8,7 +10,7 @@ let handler = async (m, { conn }) => {
     // Sacamos la foto
     let pp = await conn.profilePictureUrl(who, 'image').catch(() => defaultAvatar)
 
-    // FORZAMOS A QUE SEA STRING. Si no es, usamos la default
+    // Validamos que sea string
     if (typeof pp!== 'string' ||!pp ||!pp.startsWith('http')) {
         pp = defaultAvatar
     }
